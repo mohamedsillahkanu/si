a1bc70827cd94809af872c1f991227955dc58aba
mohamedsillahkanu <114839447+mohamedsillahkanu@users.noreply.github.com>
2024-04-24 23:05:41 +0000

    Update appoxy.py

--- appoxy.py
+++ appoxy.py
@@ -46,7 +46,7 @@ def predict_diabetes(user_input, model):
 st.title("Diabetes Prediction App")
 
 # Load pre-trained model
-url = '/workspaces/si/model_1.pkl'
+url = 'https://github.com/mohamedsillahkanu/si/blob/main/model_1.pkl'
 model = joblib.load(url)
 
 # Define a function to reset selections
