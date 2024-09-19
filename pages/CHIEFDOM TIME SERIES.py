6fcee78348c75fe21fe38fe621eacd3fc6498ed1
mohamedsillahkanu <114839447+mohamedsillahkanu@users.noreply.github.com>
2024-09-19 09:43:26 -0500

    Update CHIEFDOM TIME SERIES.py

--- pages/CHIEFDOM TIME SERIES.py
+++ pages/CHIEFDOM TIME SERIES.py
@@ -12,26 +12,7 @@ from io import BytesIO
 from matplotlib import pyplot as plt
 import pandas as pd
 
-st.markdown("""
-    <style>
-    .big-font {
-        font-size:50px !important;
-    }
-    .bold-red {
-        color: red;
-        font-weight: bold;
-    }
-    .highlight {
-        background-color: yellow;
-        color: black;
-    }
-    </style>
-""", unsafe_allow_html=True)
-
-# Applying custom classes
-st.markdown('<p class="big-font">This text is very large!</p>', unsafe_allow_html=True)
-st.markdown('<p class="bold-red">This text is bold and red!</p>', unsafe_allow_html=True)
-st.markdown('<p class="highlight">This text has a highlighted background!</p>', unsafe_allow_html=True)
+st.set_page_config(layout="wide")
 
 
 # Function to load and process the shapefile
