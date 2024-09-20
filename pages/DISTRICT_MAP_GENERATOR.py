0e249f1b6fcac6f40f6186cf0e31b1157d60baa6
mohamedsillahkanu <114839447+mohamedsillahkanu@users.noreply.github.com>
2024-09-19 19:35:22 -0500

    Update DISTRICT_MAP_GENERATOR.py

--- pages/DISTRICT_MAP_GENERATOR.py
+++ pages/DISTRICT_MAP_GENERATOR.py
@@ -34,7 +34,7 @@ if uploaded_file is not None:
     image_name = st.text_input("Image Name:", value="map_image")
     font_size = st.slider("Font Size (for Map Title):", min_value=8, max_value=24, value=15)
 
-    st.image("icf_sl (1).jpg", caption="MAP GENERATOR", use_column_width=True)
+    st.image("Color palette.png", caption="COLOR PALETTE", use_column_width=True)
     color_palette_name = st.selectbox("Color Palette:", options=list(plt.colormaps()), index=list(plt.colormaps()).index('Set3'))
 
     line_color = st.selectbox("Select Default Line Color:", options=["White", "Black", "Red"], index=1)
